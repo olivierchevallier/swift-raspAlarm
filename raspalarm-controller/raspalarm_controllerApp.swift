5a3c690f9430de4c1d3c131611d060e7c6791c84
Olivier Chevallier <olivier.chevallier@me.com>
2020-08-16 12:57:37 +0200

    Initial Commit

---
+++ raspalarm-controller/raspalarm_controllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  raspalarm_controllerApp.swift
+//  raspalarm-controller
+//
+//  Created by Olivier Chevallier on 16.08.20.
+//
+
+import SwiftUI
+
+@main
+struct raspalarm_controllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
